ef85ff2070dabac9af019a4cd9eec507ede7712d
Edward Summitt <eddiesummitt1645@gmail.com>
2026-09-15 16:10:59 -0500

    fixed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,29 +4,17 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flooter/main.dart';
 
 void main() {
-  testWidgets('Counter supports increment, decrement, and reset', (WidgetTester tester) async {
+  testWidgets('Counter decrements three times when minus is pressed', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
-    await tester.tap(find.byIcon(Icons.add));
+    final minusButton = find.byIcon(Icons.remove);
+    await tester.tap(minusButton);
+    await tester.tap(minusButton);
+    await tester.tap(minusButton);
     await tester.pump();
 
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-
-    await tester.tap(find.byIcon(Icons.remove));
-    await tester.pump();
-
-    expect(find.text('1'), findsNothing);
-    expect(find.text('0'), findsOneWidget);
-
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-    await tester.tap(find.byIcon(Icons.exposure_zero));
-    await tester.pump();
-
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('-3'), findsOneWidget);
   });
 }
